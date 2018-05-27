893b8ad68e9e3254d08e723c0bf8aebaf5a146b9
Javidx9 <javidx9yt@gmail.com>
2018-05-27 17:50:09 +0100

    Added Racing Lines Video

--- olcConsoleGameEngine.h
+++ olcConsoleGameEngine.h
@@ -51,9 +51,9 @@ Beginners Guide:		https://youtu.be/u5BhrA8ED0o
 Shout Outs!
 ~~~~~~~~~~~
 Thanks to cool people who helped with testing, bug-finding and fixing!
-wowLinh, JavaJack59, idkwid, kingtatgi, Return Null, CPP Guy
+wowLinh, JavaJack59, idkwid, kingtatgi, Return Null, CPP Guy, MaGetzUb
 
-Last Updated: 18/03/2018
+Last Updated: 27/05/2018
 
 Usage:
 ~~~~~~
@@ -370,13 +370,13 @@ public:
 		cfi.FontFamily = FF_DONTCARE;
 		cfi.FontWeight = FW_NORMAL;
 
-		/*DWORD version = GetVersion();
+	/*	DWORD version = GetVersion();
 		DWORD major = (DWORD)(LOBYTE(LOWORD(version)));
-		DWORD minor = (DWORD)(HIBYTE(LOWORD(version)));
+		DWORD minor = (DWORD)(HIBYTE(LOWORD(version)));*/
 
-		if ((major > 6) || ((major == 6) && (minor >= 2) && (minor < 4)))		
-			wcscpy_s(cfi.FaceName, L"Raster"); // Windows 8 :(
-		else*/
+		//if ((major > 6) || ((major == 6) && (minor >= 2) && (minor < 4)))		
+		//	wcscpy_s(cfi.FaceName, L"Raster"); // Windows 8 :(
+		//else
 			wcscpy_s(cfi.FaceName, L"Lucida Console"); // Everything else :P
 
 		//wcscpy_s(cfi.FaceName, L"Liberation Mono");
@@ -461,27 +461,18 @@ public:
 	void DrawLine(int x1, int y1, int x2, int y2, wchar_t c = 0x2588, short col = 0x000F)
 	{
 		int x, y, dx, dy, dx1, dy1, px, py, xe, ye, i;
-		dx = x2 - x1;
-		dy = y2 - y1;
-		dx1 = abs(dx);
-		dy1 = abs(dy);
-		px = 2 * dy1 - dx1;
-		py = 2 * dx1 - dy1;
+		dx = x2 - x1; dy = y2 - y1;
+		dx1 = abs(dx); dy1 = abs(dy);
+		px = 2 * dy1 - dx1;	py = 2 * dx1 - dy1;
 		if (dy1 <= dx1)
 		{
 			if (dx >= 0)
-			{
-				x = x1;
-				y = y1;
-				xe = x2;
-			}
+				{ x = x1; y = y1; xe = x2; }
 			else
-			{
-				x = x2;
-				y = y2;
-				xe = x1;
-			}
+				{ x = x2; y = y2; xe = x1;}
+
 			Draw(x, y, c, col);
+			
 			for (i = 0; x<xe; i++)
 			{
 				x = x + 1;
@@ -489,10 +480,7 @@ public:
 					px = px + 2 * dy1;
 				else
 				{
-					if ((dx<0 && dy<0) || (dx>0 && dy>0))
-						y = y + 1;
-					else
-						y = y - 1;
+					if ((dx<0 && dy<0) || (dx>0 && dy>0)) y = y + 1; else y = y - 1;
 					px = px + 2 * (dy1 - dx1);
 				}
 				Draw(x, y, c, col);
@@ -501,18 +489,12 @@ public:
 		else
 		{
 			if (dy >= 0)
-			{
-				x = x1;
-				y = y1;
-				ye = y2;
-			}
+				{ x = x1; y = y1; ye = y2; }
 			else
-			{
-				x = x2;
-				y = y2;
-				ye = y1;
-			}
+				{ x = x2; y = y2; ye = y1; }
+
 			Draw(x, y, c, col);
+
 			for (i = 0; y<ye; i++)
 			{
 				y = y + 1;
@@ -520,16 +502,199 @@ public:
 					py = py + 2 * dx1;
 				else
 				{
-					if ((dx<0 && dy<0) || (dx>0 && dy>0))
-						x = x + 1;
-					else
-						x = x - 1;
+					if ((dx<0 && dy<0) || (dx>0 && dy>0)) x = x + 1; else x = x - 1;
 					py = py + 2 * (dx1 - dy1);
 				}
 				Draw(x, y, c, col);
 			}
 		}
 	}
+
+	void DrawTriangle(int x1, int y1, int x2, int y2, int x3, int y3, wchar_t c = 0x2588, short col = 0x000F)
+	{
+		DrawLine(x1, y1, x2, y2, c, col);
+		DrawLine(x2, y2, x3, y3, c, col);
+		DrawLine(x3, y3, x1, y1, c, col);
+	}
+
+	// https://www.avrfreaks.net/sites/default/files/triangles.c
+	void FillTriangle(int x1, int y1, int x2, int y2, int x3, int y3, wchar_t c = 0x2588, short col = 0x000F)
+	{
+		auto SWAP = [](int &x, int &y) { int t = x; x = y; y = t; };
+		auto drawline = [&](int sx, int ex, int ny) { for (int i = sx; i <= ex; i++) Draw(i, ny, c, col); };
+		
+		int t1x, t2x, y, minx, maxx, t1xp, t2xp;
+		bool changed1 = false;
+		bool changed2 = false;
+		int signx1, signx2, dx1, dy1, dx2, dy2;
+		int e1, e2;
+		// Sort vertices
+		if (y1>y2) { SWAP(y1, y2); SWAP(x1, x2); }
+		if (y1>y3) { SWAP(y1, y3); SWAP(x1, x3); }
+		if (y2>y3) { SWAP(y2, y3); SWAP(x2, x3); }
+
+		t1x = t2x = x1; y = y1;   // Starting points
+		dx1 = (int)(x2 - x1); if (dx1<0) { dx1 = -dx1; signx1 = -1; }
+		else signx1 = 1;
+		dy1 = (int)(y2 - y1);
+
+		dx2 = (int)(x3 - x1); if (dx2<0) { dx2 = -dx2; signx2 = -1; }
+		else signx2 = 1;
+		dy2 = (int)(y3 - y1);
+
+		if (dy1 > dx1) {   // swap values
+			SWAP(dx1, dy1);
+			changed1 = true;
+		}
+		if (dy2 > dx2) {   // swap values
+			SWAP(dy2, dx2);
+			changed2 = true;
+		}
+
+		e2 = (int)(dx2 >> 1);
+		// Flat top, just process the second half
+		if (y1 == y2) goto next;
+		e1 = (int)(dx1 >> 1);
+
+		for (int i = 0; i < dx1;) {
+			t1xp = 0; t2xp = 0;
+			if (t1x<t2x) { minx = t1x; maxx = t2x; }
+			else { minx = t2x; maxx = t1x; }
+			// process first line until y value is about to change
+			while (i<dx1) {
+				i++;
+				e1 += dy1;
+				while (e1 >= dx1) {
+					e1 -= dx1;
+					if (changed1) t1xp = signx1;//t1x += signx1;
+					else          goto next1;
+				}
+				if (changed1) break;
+				else t1x += signx1;
+			}
+			// Move line
+		next1:
+			// process second line until y value is about to change
+			while (1) {
+				e2 += dy2;
+				while (e2 >= dx2) {
+					e2 -= dx2;
+					if (changed2) t2xp = signx2;//t2x += signx2;
+					else          goto next2;
+				}
+				if (changed2)     break;
+				else              t2x += signx2;
+			}
+		next2:
+			if (minx>t1x) minx = t1x; if (minx>t2x) minx = t2x;
+			if (maxx<t1x) maxx = t1x; if (maxx<t2x) maxx = t2x;
+			drawline(minx, maxx, y);    // Draw line from min to max points found on the y
+										 // Now increase y
+			if (!changed1) t1x += signx1;
+			t1x += t1xp;
+			if (!changed2) t2x += signx2;
+			t2x += t2xp;
+			y += 1;
+			if (y == y2) break;
+
+		}
+	next:
+		// Second half
+		dx1 = (int)(x3 - x2); if (dx1<0) { dx1 = -dx1; signx1 = -1; }
+		else signx1 = 1;
+		dy1 = (int)(y3 - y2);
+		t1x = x2;
+
+		if (dy1 > dx1) {   // swap values
+			SWAP(dy1, dx1);
+			changed1 = true;
+		}
+		else changed1 = false;
+
+		e1 = (int)(dx1 >> 1);
+
+		for (int i = 0; i <= dx1; i++) {
+			t1xp = 0; t2xp = 0;
+			if (t1x<t2x) { minx = t1x; maxx = t2x; }
+			else { minx = t2x; maxx = t1x; }
+			// process first line until y value is about to change
+			while (i<dx1) {
+				e1 += dy1;
+				while (e1 >= dx1) {
+					e1 -= dx1;
+					if (changed1) { t1xp = signx1; break; }//t1x += signx1;
+					else          goto next3;
+				}
+				if (changed1) break;
+				else   	   	  t1x += signx1;
+				if (i<dx1) i++;
+			}
+		next3:
+			// process second line until y value is about to change
+			while (t2x != x3) {
+				e2 += dy2;
+				while (e2 >= dx2) {
+					e2 -= dx2;
+					if (changed2) t2xp = signx2;
+					else          goto next4;
+				}
+				if (changed2)     break;
+				else              t2x += signx2;
+			}
+		next4:
+
+			if (minx>t1x) minx = t1x; if (minx>t2x) minx = t2x;
+			if (maxx<t1x) maxx = t1x; if (maxx<t2x) maxx = t2x;
+			drawline(minx, maxx, y);   										
+			if (!changed1) t1x += signx1;
+			t1x += t1xp;
+			if (!changed2) t2x += signx2;
+			t2x += t2xp;
+			y += 1;
+			if (y>y3) return;
+		}
+	}
+
+	// Non-bresenham method
+	/*void FillTriangle(int x1, int y1, int x2, int y2, int x3, int y3, wchar_t c = 0x2588, short col = 0x000F)
+	{
+		if ((x1 == x2 && x1 == x3) || (y1 == y2) && (y1 == y3))
+			return;
+		
+		int tmp;
+		if (y2 < y1){ tmp = y1; y1 = y2; y2 = tmp; tmp = x1; x1 = x2; x2 = tmp;	}
+		if (y3 < y1){ tmp = y1; y1 = y3; y3 = tmp; tmp = x1; x1 = x3; x3 = tmp;	}
+		if (y3 < y2){ tmp = y2;	y2 = y3; y3 = tmp; tmp = x2; x2 = x3; x3 = tmp;	}
+
+		int dy1 = y2 - y1, dx1 = x2 - x1;
+		int dy2 = y3 - y1, dx2 = x3 - x1;
+
+		if (dy1) 
+		{
+			for (int i = y1; i < y2; i++) 
+			{
+				int ax = x1 + ((i - y1)*dx1) / dy1;
+				int bx = x1 + ((i - y1)*dx2) / dy2;
+				tmp = ax;
+				if (ax > bx) ax = bx, bx = tmp;
+				for (int j = ax; j < bx; j++) Draw(j, i, c, col);
+			}
+		}
+
+		dy1 = y3 - y2; dx1 = x3 - x2;
+
+		if (dy1) 
+		{
+			for (int i = y2; i < y3; i++) 
+			{
+				int ax = x2 + ((i - y2)*dx1) / dy1;
+				int bx = x1 + ((i - y1)*dx2) / dy2;
+				tmp = ax;
+				if (ax > bx) ax = bx, bx = tmp;
+				for (int j = ax; j < bx; j++) Draw(j, i, c, col);
+			}
+		}
+	}*/
 
 	void DrawCircle(int xc, int yc, int r, wchar_t c = 0x2588, short col = 0x000F)
 	{
