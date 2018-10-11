56bc10697651417522c89feae8fc8da41a7fd6f4
Javidx9 <javidx9yt@gmail.com>
2018-10-11 15:47:33 +0100

    Fixed GLOOP::PlaySample Def

--- olcConsoleGameEngineGLOOP.cpp
+++ olcConsoleGameEngineGLOOP.cpp
@@ -1413,7 +1413,7 @@ unsigned int olcConsoleGameEngineGLOOP::LoadAudioSample(std::wstring sWavFile)
 }
 
 // Add sample 'id' to the mixers sounds to play list
-void olcConsoleGameEngineOOP::PlaySample(int id, bool bLoop = false)
+void olcConsoleGameEngineGLOOP::PlaySample(int id, bool bLoop = false)
 {
 	sCurrentlyPlayingSample a;
 	a.nAudioSampleID = id;
